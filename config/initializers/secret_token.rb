0857b4d58130531d3934e7a461581442ebbcdf3a
bhighnam <bhighnam@gmail.com>
2014-01-31 13:44:48 -0500

    Brian Highnam testing...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Teamawesome::Application.config.secret_key_base = '228d5570bdbe1ade69174f7b26ba36602e6f5fe3c500b73353a475ba8a81f5d5f0a96cc89742214f626996e2c268e04ebc682d6ee6cef74f27a7fe55e353c350'
