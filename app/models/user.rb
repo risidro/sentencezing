7e015d12062d195c0ca138c5a76e71363c7e053b
mwatkin3 <Michael@Michaels-Laptop.home>
2014-02-24 16:07:55 -0500

    Email validation change

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   validates :email, :password, :presence => true
-  validates :email, :format => {:with => /^([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})$/i}
+  validates :email, :format => {:with => [-0-9a-zA-Z.+_]+@[-0-9a-zA-Z.+_]+\.[a-zA-Z]{2,4}
   validates :email, :uniqueness => true
   validates :password, :length => {:minimum => 6}
   has_many :microposts
